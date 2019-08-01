3e7b7614fe8dced85432d8c44b7386163dc515ce
Rachel T <rachmaybe@gmail.com>
2019-08-01 23:40:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(hp_characters, dumbledore)
   [hp_characters = "Harry", "Hermione", "Ron", "Malfoy"]
-  hp_characters << "dumbledore"
+  hp_characters << "array"
   p hp_characters
 end
 
