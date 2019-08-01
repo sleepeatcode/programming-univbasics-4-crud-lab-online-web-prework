40cb6e15b690587c8cedcad0aa69aaccae65b2ec
Rachel T <rachmaybe@gmail.com>
2019-08-01 23:27:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  hp_characters = ["Harry", "Hermione", "Ron", "Malfoy"]
   hp_characters.push (Dumbledore)
+  p hp_characters
 end
 
 def add_element_to_start_of_array(array, element)
