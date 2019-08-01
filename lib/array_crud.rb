cb07e62c3e5d8de2b4ff897e1c70150bbb752296
Rachel T <rachmaybe@gmail.com>
2019-08-01 23:48:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  hp_characters=["Harry","Hermione", "am", "Ron", "Malfoy"]
+  hp_characters=["Harry","Hermione", "am", "Malfoy"]
   hp_characters = [2]
 end
 
