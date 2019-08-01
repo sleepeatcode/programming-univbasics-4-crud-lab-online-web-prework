9715d7292dab981f3fbdf0ac8eb9635ae290701b
Rachel T <rachmaybe@gmail.com>
2019-08-01 23:42:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   hp_characters=["Harry","Hermione","Ron", "Malfoy"]
-  famous_cats.unshift (arrays!)
+  hp_characters.unshift (Dumbledore!)
 end
 
 def remove_element_from_end_of_array(array)
