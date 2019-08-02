4e5379760f33c2ae6546c6bce5447b2437909a40
Rachel T <rachmaybe@gmail.com>
2019-08-02 01:05:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,49 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ hp_characters = ["Harry", "Hermione", "Ron", "Malfoy"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  hp_characters = ["Harry", "Hermione", "Ron", "Malfoy"]
+  hp_characters << "arrays!"
+  p hp_characters
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  hp_characters=["Harry","Hermione","Ron", "Malfoy"]
+  hp_characters.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ hp_characters=["wow","Harry","Hermione","Ron", "Malfoy","arrays!"]
+hp_characters.pop
 end
 
+
 def remove_element_from_start_of_array(array)
-  
+ hp_characters=["wow","Harry","am","Ron", "Malfoy","arrays!"]
+ wow = hp_characters.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    hp_characters=["wow","Harry","am","Ron", "Malfoy","arrays!"]
+    hp_characters[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+   hp_characters=["wow","Harry","am","Ron", "Malfoy","arrays!"]
+   hp_characters[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+   hp_characters=["wow","Harry","am","Ron", "Malfoy","arrays!"]
+   hp_characters[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+ hp_characters=["wow","Harry","am","Ron", "Malfoy","arrays!"]
+ hp_characters[4]="totally"
 end
